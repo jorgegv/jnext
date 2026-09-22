edf631c46845550e825d9c175ef7cb4caa350c3b
ZXjogv <zx@jogv.es>
2026-09-22 07:44:09 +0200

    test: label tap_loader_test in the subsystem-status dashboard

--- test/refresh-subsystem-status.sh
+++ test/refresh-subsystem-status.sh
@@ -143,6 +143,7 @@ BEGIN {
     M["nex_loader_test"]               = "NEX loader (screen ingest)"
     M["nex_v13_test"]                  = "NEX loader (V1.3)"
     M["extended_nex_test"]             = "Extended NEX streaming"
+    M["tap_loader_test"]               = "TAP loader (container)"
     M["dma_test"]                      = "DMA"
     M["tilemap_test"]                  = "Tilemap"
     M["tilemap_fetch_split_test"]      = "Tilemap raster splits"
